8c38602be3e0671ee527d8d685facd392124024b
Neha Nayak <nnayak@umass.edu>
2020-08-31 01:22:34 -0400

    characterizing sequences

--- comment_types.py
+++ comment_types.py
@@ -16,21 +16,10 @@ def dict_factory(cursor, row):
     return d
 
 
-class CommentTypes(object):
-  ROOT = "root"
-  REVIEW = "review"
-  METAREVIEW = "probably metareview"
-  METARESPONSE = "probably metaresponse"
-  ANON_TOP = "anonymous top"
-  NAMED_TOP = "named top"
-  REBUTTAL = "rebuttal"
-  INTER_REVIEWER = "inter-reviewer"
-  AUTHOR_TO_REVIEWER = "author-to-reviewer"
-  REVIEWER_TO_AUTHOR = "reviewer-to-author"
-  AC_INTERJECTION = "ac interjection"
-  ANON_INTERJECTION = "anonymous interjection"
-  NAMED_INTERJECTION = "named interjection"
-  UNOFFICIAL_CONVERSATION = "unofficial conversation"
+THREAD_PROFILES = {
+    # Thread profiles are for two participants
+    "review-rebuttal":  ["root", "reviewer", "author"]
+    }
 
 def is_by_author(comment):
   if "Author" in comment.author:
@@ -49,31 +38,120 @@ def is_by_anonymous(comment):
 def is_by_ac(comment):
   return "Area_Chair" in comment.author
 
+def is_by_conference(comment):
+  return comment.author == "ICLR.cc/2019/Conference"
+
+def shorten_author(comment):
+  if is_by_author(comment):
+    return Participants.AUTHOR 
+  elif is_by_reviewer(comment):
+    return Participants.REVIEWER
+  elif is_by_ac(comment):
+    return Participants.AC
+  elif is_by_anonymous(comment):
+    anon_dsds
+    return "Anon"
+  elif is_by_conference(comment):
+    return Participants.CONFERENCE
+  else:
+    named_dsds
+    return "Named"
+
+
 def is_official(comment):
-  return is_by_ac(comment) or is_by_reviewer(comment) or is_by_author(comment)
+  return (is_by_ac(comment) or is_by_reviewer(comment)
+      or is_by_author(comment) or "Conference" in comment.author)
 
-def comment_type_trail(child, comment_map, comment_type_map):
-  ancestors = []
-  curr_ancestor = child
-  while not curr_ancestor == "None":
-    if curr_ancestor in comment_type_map:
-      ancestors.append(comment_type_map[curr_ancestor])
+
+class Participants(object):
+  CONFERENCE = "Conference"
+  AUTHOR = "Author"
+  AC = "AC"
+  REVIEWER = "Reviewer"
+  REVIEWER_B = "Reviewer B"
+  MULTIPLE = "Multiple"
+
+
+def shorten_author(author):
+  if Participants.AUTHOR in author:
+    return Participants.AUTHOR
+  elif Participants.REVIEWER in author:
+    return Participants.REVIEWER
+  elif "Area_Chair" in author:
+    return Participants.AC
+  elif Participants.CONFERENCE in author:
+    return Participants.CONFERENCE
+  else:
+    dsds
+
+
+def is_reviewer(author):
+  return "AnonReviewer" in author
+
+def shorten_sequence(sequence):
+  new_sequence = []
+  for author in sequence:
+    if is_reviewer(author):
+      if Participants.REVIEWER in new_sequence:
+        new_sequence.append(Participants.REVIEWER_B)
+      else:
+        new_sequence.append(Participants.REVIEWER)
     else:
-      ancestors.append(comment_map[curr_ancestor].author)
-    curr_ancestor = comment_map[curr_ancestor].parent_id
+      new_sequence.append(shorten_author(author))
+  return new_sequence
 
-  return "\t".join(reversed(ancestors))
-
-def check_thread_participants(child, comment_map):
+  
+def characterize_path(terminal_node, comment_map):
   ancestors = []
-  curr_ancestor = child
-  while not comment_map[curr_ancestor].parent_id == "None":
+  curr_ancestor = terminal_node
+  while not curr_ancestor == "None":
     ancestors.append(comment_map[curr_ancestor].author)
     curr_ancestor = comment_map[curr_ancestor].parent_id
 
-  print("\t".join([
-    str(len(ancestors)), "comments", str(len(set(ancestors))),
-    "participants"]))
+  sequence = list(reversed(ancestors))
+  initiator = sequence[1]
+
+
+  participants = sequence[:2]
+  for next_participant in sequence[2:]:
+    if next_participant not in participants:
+      participants.append(next_participant)
+
+  if len(participants) > 3:
+    final = shorten_sequence(participants[:3]) + [Participants.MULTIPLE]
+  else:
+    final = shorten_sequence(participants[:4])
+
+  #print(" ".join(participants))
+  #print(" ".join(final))
+  #print("-" * 80)
+
+  return final
+
+
+
+
+def prune_unofficial(parents, comment_map):
+  children = collections.defaultdict(list)
+  for child, parent in parents.items():
+    children[parent].append(child)
+
+  official_children = collections.defaultdict(list)
+
+  queue = ["None"]
+  while queue:
+    curr_id = queue.pop(0)
+    for child in children[curr_id]:
+      if is_official(comment_map[child]):
+        queue.append(child)
+        official_children[curr_id].append(child)
+
+  official_parents = {}
+  for parent, children in official_children.items():
+    for child in children:
+      official_parents[child] = parent
+
+  return official_parents
 
 
 def main():
@@ -93,104 +171,34 @@ def main():
 
   structure_map, comment_map = ordb.crunch_structure_rows(rows)
 
-
-  comment_type_map = {}
-  comment_type_accumulator = {}
- 
-
-  # Get all root commments
-  for forum, structure in structure_map.items():
-    for child, parent in structure.items():
-      assert child not in comment_type_map
-      if parent == "None":
-        comment_type_accumulator[child] = CommentTypes.ROOT
-
-  previous_level = dict(comment_type_accumulator)
-  comment_type_map.update(comment_type_accumulator)
-  comment_type_accumulator = {}
-
-  for forum, structure in structure_map.items():
-    for child, parent in structure.items():
-      if parent in previous_level:
-        comment = comment_map[child]
-        if is_by_reviewer(comment):
-          comment_type_accumulator[child] = CommentTypes.REVIEW
-        elif is_by_author(comment):
-          comment_type_accumulator[child] = CommentTypes.METARESPONSE
-        elif is_by_ac(comment):
-          comment_type_accumulator[child] = CommentTypes.METAREVIEW
-        elif is_by_anonymous(comment):
-          comment_type_accumulator[child] = CommentTypes.ANON_TOP
-        else:
-          assert comment.author.startswith("~")
-          comment_type_accumulator[child] = CommentTypes.NAMED_TOP
-
-  assert not set(
-      previous_level.keys()).intersection(comment_type_accumulator.keys())
-
-  previous_level = dict(comment_type_accumulator)
-  comment_type_map.update(comment_type_accumulator)
-  comment_type_accumulator = {}
-
-  for forum, structure in structure_map.items():
-    for child, parent in structure.items():
-      if parent in previous_level:
-        comment = comment_map[child]
-        parent_comment = comment_map[parent]
-        if is_by_reviewer(comment):
-          if is_by_reviewer(parent_comment):
-            comment_type_accumulator[child] = CommentTypes.INTER_REVIEWER
-          elif is_by_author(parent_comment):
-            comment_type_accumulator[child] = CommentTypes.REVIEWER_TO_AUTHOR
-        elif is_by_author(comment):
-          if is_by_reviewer(parent_comment):
-            comment_type_accumulator[child] = CommentTypes.REBUTTAL
-          elif is_by_author(parent_comment):
-           assert False 
-        elif is_by_ac(comment):
-          assert not is_by_ac(parent_comment)
-          comment_type_accumulator[child] = CommentTypes.AC_INTERJECTION
-        elif is_by_anonymous(comment):
-          if is_official(parent_comment):
-            comment_type_accumulator[child] = CommentTypes.ANON_INTERJECTION
-          else:
-            comment_type_accumulator[child] = CommentTypes.UNOFFICIAL_CONVERSATION
-        else:
-          assert comment.author.startswith("~")
-          if is_official(parent_comment):
-            comment_type_accumulator[child] = CommentTypes.NAMED_INTERJECTION
-          else:
-            comment_type_accumulator[child] = CommentTypes.UNOFFICIAL_CONVERSATION
-
-  assert not set(
-      previous_level.keys()).intersection(comment_type_accumulator.keys())
-          
-  previous_level = dict(comment_type_accumulator)
-  comment_type_map.update(comment_type_accumulator)
-  comment_type_accumulator = {}
-
-  outstanding_children = []
-
-
-  all_parents, all_children = set(), set()
-
-  for structure in structure_map.values():
-    all_parents.update(structure.values())
-    all_children.update(structure.keys())
-
-  for forum, structure in structure_map.items():
-    for child, parent in structure.items():
-      if child not in comment_type_map:
-        outstanding_children.append(child)
-        #print(comment_type_trail(child, comment_map, comment_type_map))
-
-
-  non_parent_children = set(outstanding_children) - set(all_parents)
-  for c in non_parent_children:
-    check_thread_participants(c, comment_map)
+  all_nodes = set()
+  for k, v in structure_map.items():
+    all_nodes.update(set(v.keys()))
+    all_nodes.update(set(v.values()))
+  print("Total number of nodes", len(all_nodes))
   
+
+  official_structure_map = {
+      forum_id:prune_unofficial(structure, comment_map)
+      for forum_id, structure in structure_map.items()}
+
+  all_nodes = set()
+  parent_nodes = set()
+  child_nodes = set()
+  for k, v in official_structure_map.items():
+    child_nodes.update(set(v.keys()))
+    parent_nodes.update(set(v.values()))
+  print("Total number of official nodes", len(parent_nodes.union(child_nodes)))
+
+  non_parents = child_nodes - parent_nodes
+
+  print("Total number of paths", len(non_parents))
+
+  for terminal_node in non_parents:
+    print(" ".join(characterize_path(terminal_node, comment_map)))
+
+
+
+ 
 if __name__ == "__main__":
   main()
-
-
-
