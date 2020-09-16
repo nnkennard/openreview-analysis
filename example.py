413c3db93eca16d0bacdfec9a6d261675d27ee28
Neha Nayak <nnayak@umass.edu>
2020-09-16 13:06:44 -0400

    modified to start with db as input

--- example.py
+++ example.py
@@ -1,55 +1,37 @@
-import sqlite3
 import argparse
-import lib.openreview_db as ordb
+import sqlite3
+import sys
+
+import lib.db_lib as dbl
 
 parser = argparse.ArgumentParser(
-    description='Example for accessing OpenReview data')
+    description='Load OpenReview data from a sqlite3 database.')
 parser.add_argument('-d', '--dbfile', default="db/or.db",
     type=str, help='path to database file')
-
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
 
 
 def main():
 
   args = parser.parse_args()
+  conn = dbl.create_connection(args.dbfile)
+  if conn is not None:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM traindev_pairs WHERE split=? LIMIT 10",
+        ("train",))
+    rows = cur.fetchall()
+    for row in rows:
+      cur.execute("SELECT * FROM traindev WHERE comment_supernote=?",
+          (row["review_supernote"],))
+      
+      k = dbl.crunch_text_rows(cur.fetchall())
 
-  conn = ordb.create_connection(args.dbfile)
-  conn.row_factory = dict_factory
-
-
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM comments WHERE split=?", ("train",))
-
-  rows = cur.fetchall()
-  print(len(rows))
-  obj = ordb.crunch_text_rows(rows)
-  num_tokens = 0
-  num_comments = 0
-  for comment, text in obj.items():
-    if len(text) == 1: # One chunk
-      print(" ".join(sum(text[0], [])))
-      num_tokens += len(sum(text[0], []))
-      num_comments += 1
-
-
-  print(num_tokens)
-  print(num_comments)
-  print(num_tokens / num_comments)
-
-
-  #print(obj.keys())
-  #print(len(obj.keys()))
-
+      for _, chunks in k.items():
+        for chunk in chunks:
+          for sentence in chunk:
+            print(" ".join(sentence))
+          print()
+        print("*" * 80)
 
 
 if __name__ == "__main__":
   main()
-
-
-
